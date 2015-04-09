74c08b5e802c934c32dff6b9bec1b39997b12c07
jeydlex <alexebarahona@yahoo.es>
2015-04-09 21:33:10 +0800

    changed to rotate because shuffle did not work

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -9,15 +9,17 @@ module DoubleTranspositionCipher
      doc_blocks = document.split('').map(&:to_s).each_slice(cols).to_a
      
      # 3. sort rows in predictibly random way using key as seed
-     new_blocks = doc_blocks.shuffle(random: Random.new(key))
-
+     #new_blocks = doc_blocks.shuffle(random: Random.new(key))
+	 new_blocks = doc_blocks.rotate(key)
+	 
      #4. sort columns of each row in predictibly random way
 	
 	 # 4.1. transpose columns and make them array	
 	 sorted_columns = (new_blocks.transpose()).to_a 
 	
 	 # 4.2 this assigns a random order to the columns based on the key	
-	 new_sorted_columns = sorted_columns.shuffle(random: Random.new(key))
+	 #new_sorted_columns = sorted_columns.shuffle(random: Random.new(key))
+	 new_sorted_columns = sorted_columns.rotate(key)
 	 
 	 # 4.3. final matrix
 	 final_matrix = (new_sorted_columns.transpose()).to_a	 
@@ -37,13 +39,15 @@ module DoubleTranspositionCipher
      doc_blocks_mat = (doc_blocks.transpose()).to_a
      
      #4. sort columns of each row in predictibly random way -SHUFFLE IS NOT PREDICTABLE, IT IS RANDOM
-	 new_blocks = doc_blocks_mat.shuffle(random: Random.new(key))
+	 #new_blocks = doc_blocks_mat.shuffle(random: Random.new(key))
+	 new_blocks = doc_blocks_mat.rotate(-key)
 	
 	 # 4.1. transpose columns and make them array	
 	 original_end = (new_blocks.transpose()).to_a
 	
 	 # 4.2 this assigns a random order to the columns based on the key	
-	 original_doc = original_end.shuffle(random: Random.new(key))
+	 #original_doc = original_end.shuffle(random: Random.new(key))
+	 original_doc = original_end.rotate(-key)
 	 return original_doc.join
   end
 end
