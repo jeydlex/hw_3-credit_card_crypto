41a30d609031a32cbdfad76afd8a8314b6de7669
rojotic26 <rojotic26@gmail.com>
2015-04-09 18:32:56 +0800

    RUbocop

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,27 +1,27 @@
 module DoubleTranspositionCipher
   def self.encrypt(document, key)
-     # 1. find number of rows/cols such that matrix is almost square
-     
-     rows = Math.sqrt(document.length).round #for making almost a perfect square
-     cols = (document.length/rows.to_f).round #for finding the number of columns of the matrix
+    # 1. find number of rows/cols such that matrix is almost square
 
-     # 2. break plaintext into evenly sized blocks
-     doc_blocks = document.split('').map(&:to_s).each_slice(cols).to_a
+    rows = Math.sqrt(document.length).round # for making almost a perfect square
+    cols = (document.length / rows.to_f).round # for finding the number of columns of the matrix
 
-     # 3. sort rows in predictibly random way using key as seed
-     new_blocks = doc_blocks.shuffle(random: Random.new(key))
+    # 2. break plaintext into evenly sized blocks
+    doc_blocks = document.split('').map(&:to_s).each_slice(cols).to_a
 
-     # 4. sort columns of each row in predictibly random way
+    # 3. sort rows in predictibly random way using key as seed
+    new_blocks = doc_blocks.shuffle(random: Random.new(key))
 
-     # 4.1. transpose columns and make them array
-     sorted_columns = (new_blocks.transpose).to_a
+    # 4. sort columns of each row in predictibly random way
 
-     # 4.2 this assigns a random order to the columns based on the key
-     new_sorted_columns = sorted_columns.shuffle(random: Random.new(key))
+    # 4.1. transpose columns and make them array
+    sorted_columns = (new_blocks.transpose).to_a
 
-     # 4.3. final matrix
-     final_matrix = (new_sorted_columns.transpose).to_a
-     return final_matrix.join
+    # 4.2 this assigns a random order to the columns based on the key
+    new_sorted_columns = sorted_columns.shuffle(random: Random.new(key))
+
+    # 4.3. final matrix
+    final_matrix = (new_sorted_columns.transpose).to_a
+    return final_matrix.join
   end
 
   def self.decrypt(ciphertext, key)
